cc5538f185f1c78484322fa2fb6e67c540953420
Maya <mesaintv@stud.ntnu.no>
2025-03-21 14:47:19 +0100

    changed backup a bit

--- Project/pkg/backup/presistence.go
+++ Project/pkg/backup/presistence.go
@@ -1,22 +1,38 @@
 package backup
 
 import (
-	"os"
 	"encoding/json"
 	"elevatorlab/common"
+	"elevatorlab/elevio"
+	"os"
 )
 
-const backupFile = "elevator_backup.json"
+const backupFile = "backup.json"
 
+//only cab requests are saved
 func SaveCabRequests(elevator common.Elevator){
+	var cabRequests [common.N_FLOORS]bool
+	for floor := 0; floor < common.N_FLOORS; floor++ {
+		cabRequests[floor] = elevator.Requests[floor][elevio.BT_Cab]
+	}
 	data, _:= json.Marshal(elevator.Requests)
 	os.WriteFile(backupFile, data, 0644)
 }
 
+//load the saved cab requests and merge it into elevator state
 func LoadCabRequests(elevator *common.Elevator){
 	data, err:= os.ReadFile(backupFile)
 	if err != nil {
 		return
 	}
-	json.Unmarshal(data, &elevator.Requests)
+	var cabRequests [common.N_FLOORS]bool
+	if err := json.Unmarshal(data, &cabRequests); err != nil {
+		return
+	}
+	for floor := 0; floor < common.N_FLOORS; floor++ {
+		if cabRequests[floor] {
+			elevator.Requests[floor][elevio.BT_Cab] = true
+		}
+	}
 }
+	
