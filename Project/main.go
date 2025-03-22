7f1a478d9db9d9b6cb77a881b5a749870c542101
Maya <mesaintv@stud.ntnu.no>
2025-03-22 10:35:05 +0100

    fixed errors in main

--- Project/main.go
+++ Project/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"flag"
+	"strconv"
 )
 
 func main() {
@@ -19,22 +20,28 @@ func main() {
 		os.Exit(1)
 	}
 
+	ID, err := strconv.Atoi(*id)
+	if err != nil {
+		fmt.Println("Invalid ID, must be an int")
+		os.Exit(1)
+	}
+
 	localIP, err := localip.LocalIP()
 	if err != nil {
 		fmt.Println("Error getting local IP:", err)
 		os.Exit(1)
 	}
-	fmt.Printf("Elevator %s running on IP: %s\n", *id, localIP, *port)
+	fmt.Printf("Elevator %s running on IP: %s\n", *id, localIP)
 
 	//func Init(address string, numFloors int)
 	elevio.Init("localhost:" + *port, 4)
 	
-	control.InitDispatcher(*id)
+	control.InitDispatcher()
 
 	go control.StartDispatcherLoop(
+		ID,
 		control.HallCallRequestChan,
 		control.AssignedHallCallChan,
-		*id,
 	)
 
 	control.InitFSM(*id)
